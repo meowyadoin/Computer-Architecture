be34a7fc961d52bec98d064275ad59ae6885d7fa
Lambda_School_Loaner_222 <lambda_school_loaner_222@Lambda-School-Loaner-222s-MacBook-Pro.local>
2020-08-18 13:23:37 -0700

    day 1 and 2 mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,54 +1,78 @@
 """CPU functionality."""
 
 import sys
+LDI = 0b10000010
+PRN = 0b01000111
+HLT = 0b00000001
+MUL = 0b10100010
 
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        # We need 256 bytes of Ram, 8 registers, a counter, and continuous variable
-        self.ram = [None] * 256
-        self.registers = [None] * 8
-        self.running = True
+        # 8 general-purpose registers
+        self.reg = [0] * 8
+        # Program Counter, index of the current instruction
         self.pc = 0
+        # CPU has a total of 256 bytes of memory
+        self.ram = [0] * 256
+        # exit the emulator
+        self.halted = False
+        self.LDI = LDI
+        self.HLT = HLT
+        self.PRN = PRN
+        self.MUL = MUL
 
-    def ram_read(self, address):
-        # Accepts the address and returns the value from Ram
-        return(self.ram[address])    
+    # Step 2
+    # add method `ram_read()
+    def ram_read(self, pc):
+        return self.ram[pc]
+    # Step 2
+    # add ram_write()
+    def ram_write(self, pc, value):
+        self.ram[pc] = value
 
-    def ram_write(self, value, address):
-        # Accepts a value and address and assigns to Ram
-        self.ram[address] = value
-
-
-    def load(self):
+    def load(self, program_name):
         """Load a program into memory."""
 
-        address = 0
+        address = 0 
+
+        with open(program_name) as p:
+            # for address, line in enumerate(p):
+            for line in p:
+                line_split = line.split("#")
+                num = line_split[0].strip()
+
+                if num == "":
+                    continue
+
+                try:
+                    instruction = int(num, 2)
+                except ValueError:
+                    continue
+
+
+                self.ram[address] = instruction
+                address += 1
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
-            self.registers[reg_a] += self.registers[reg_b]
+            self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -69,30 +93,62 @@ class CPU:
         ), end='')
 
         for i in range(8):
-            print(" %02X" % self.registers[i], end='')
-
+            print(" %02X" % self.reg[i], end='')
         print()
 
     def run(self):
         """Run the CPU."""
-        LDI = 0b10000010 # Sets the Value of a register to an int
-        PRN = 0b01000111 # Prints the value at the next register
-        HLT = 0b00000001 # halts the program, "ends it"/"stops it"
+        running = True
+        # while loop
+        # LDI
+        # PRN
+        # and HLT
+        while running == True:
+            ir = self.ram[self.pc]
+            # >> means shift right
+            number_of_bytes = (ir & 0b11000000) >> 6
+            # if num of bytes == 2
+            # then read both address 1 & address 2
+            if number_of_bytes == 2:
+                address_1 = self.ram[self.pc+1]
+                address_2 = self.ram[self.pc+2]
+            elif number_of_bytes == 1:
+                address_1 = self.ram[self.pc+1]
+                address_2 = None
 
-        while self.running:
-            # The HLT instruction
-            if self.ram[self.pc] == HLT:
-                self.running = False
+            if ir == self.LDI:
+                self.ldi(address_1, address_2) 
 
-            # The LDI instruction
-            if self.ram[self.pc] == LDI:
-                num_to_load = self.ram[self.pc + 2]
-                reg_index = self.ram[self.pc + 1]
-                self.registers[reg_index] = num_to_load
-                self.pc += 3
+            elif ir == self.PRN:
+                self.prn(address_1)                
 
-            # The PRN instruction
-            if self.ram[self.pc] == PRN:
-                reg_to_print = self.ram[self.pc + 1]
-                print(self.registers[reg_to_print])
-                self.pc += 2
+            elif ir == self.MUL:
+                self.mul(address_1, address_2)                
+
+            elif ir == self.HLT:
+                running = self.hlt(running)
+
+            self.pc += number_of_bytes + 1
+
+        # LDI, PRN, HLT defs
+        # LDI will have address, value, and ram_write
+        # PRN will have address and value of ram_read
+        # HLT will return false
+
+    def ldi(self, address_1, value):        
+        self.reg[address_1] = value       
+
+    def prn(self, address):        
+        value = self.reg[address]
+        print(value)        
+
+    def hlt(self, running):
+
+        return False
+
+    def mul(self, address_1, address_2):        
+
+        value_1 = self.reg[address_1]
+        value_2 = self.reg[address_2]
+        self.reg[address_1] = value_1 * value_2
+        print(value_1, value_2)
